d1f08b98de5bab5f1966f7a499e5615473390e28
Kris Arnold <karnold@imamuseum.org>
2012-09-21 22:16:10 +0000

    updated templates to properly utilize django.templates

--- RaspBEERyPi/urls.py
+++ RaspBEERyPi/urls.py
@@ -13,6 +13,6 @@ urlpatterns = patterns('',
     # url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
 
     url(r'^admin/', include(admin.site.urls)),
-    url(r'^$', direct_to_template, {"template": "index.html"}),
+    url(r'^$', 'beer.views.index'),
     url(r'^current-reading/$', 'beer.views.current_reading'),
 )
--- beer/views.py
+++ beer/views.py
@@ -1,8 +1,17 @@
 from django.shortcuts import get_object_or_404, render_to_response
 from django.http import HttpResponseRedirect, HttpResponse
+from django.template import Context, loader
 from django.utils import simplejson
 from beer.models import Reading, Batch
 from beer import sensors
+
+def index(request):
+    t = loader.get_template('index.html')
+    c = Context({
+        'title': 'Current Reading',
+    });
+
+    return HttpResponse(t.render(c))    
 
 def current_reading(request):
     s = sensors.sensors()
